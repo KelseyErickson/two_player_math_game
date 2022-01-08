45bcfce51fa8bb5bb31be8f2bd1c546854702901
Kelsey Erickson <kerickson@gmx.com>
2022-01-08 21:09:43 +0000

    Add I/O to the game

---
+++ game.rb
@@ -0,0 +1,14 @@
+class Game
+ 
+  def initialize
+    @player1 = Player.new
+    @player2 = Player.new
+  end
+
+   # Keep track of score
+
+   # ask a question and take in input (I/O)
+
+   # make the game run until game over
+
+end
