d9af077401e6dc9646fbcd6bd6a03143e7c378e7
Kelsey Erickson <kerickson@gmx.com>
2022-01-08 22:16:36 +0000

    Add basic question asking and answering

--- game.rb
+++ game.rb
@@ -10,7 +10,22 @@ class Game
     puts "----- NEW TURN -----"
   end
 
-   # ask a question and take in input (I/O)
+  def ask_question
+  
+    question = Question.new
+
+    "Player 1: #{question.question_generator}" : "Player 2: #{question.question_generator}"
+  
+    user_answer = gets.chomp.to_i
+
+    if(user_answer == question.answer)
+      puts "YES! You are correct"
+    else
+      puts "Nope"
+      @player.adjust_score
+    end
+
+  end
 
    # make the game run until game over
 
