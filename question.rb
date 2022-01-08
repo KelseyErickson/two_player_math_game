c823c95889e3a03a57b4d7ffd72ef705f9fba060
Kelsey Erickson <kerickson@gmx.com>
2022-01-08 20:58:26 +0000

    Add ability to have random operators

--- question.rb
+++ question.rb
@@ -1,11 +1,18 @@
 class Question
 
   def initialize
+    @operators = [:+, :-, :*] # not adding division - too difficult for people to do in their head
     @number1 = rand(1..20)
     @number2 = rand(1..20)
+    @operator = @operators.sample
   end
 
   def question_generator
-    "What does #{@number1} plus #{@number2} equal?"
+    "What does #{@number1} #{@operator} #{@number2} equal?"
   end
+
+  def answer
+    @number1.send(@operator, @number2) #nvokes the method identified by symbol - https://apidock.com/ruby/Object/send
+  end
+
 end
